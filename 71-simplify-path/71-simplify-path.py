9488451aefe86fdd3fa2a93f721777615363d33e
prudhvinik <prudhvikrishna.n16@iiits.in>
2022-01-20 17:52:47 +0530

    Time: 57 ms (15.54%), Space: 14.2 MB (72.67%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,10 +1,10 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         
-        
+        path = path.split("/")
         stack = []
         
-        for i in path.split("/"):
+        for i in path:
             if(stack and i == ".."):
                 stack.pop()
             elif i not in ['.','','..']:
