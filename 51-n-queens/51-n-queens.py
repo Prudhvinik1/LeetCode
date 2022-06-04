8399742aa25bb3d8a9fbe14dacf0d264017a34e6
prudhvinik <prudhvikrishna.n16@iiits.in>
2022-06-05 00:47:42 +0530

    Time: 116 ms (40.44%), Space: 14.5 MB (45.52%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,47 @@
+class Solution:
+    
+    def isSafe(self,board,row,column,n):
+        #Check along the rows
+        for i in range(n):
+            if(board[row][i] == "Q"):
+                return False
+        
+        #Check Upper Diagonal
+        i,j = row,column
+        while i>=0 and j >=0:
+            if(board[i][j] == "Q"):
+                return False
+            i -= 1
+            j -= 1
+        
+        #Check Lower Diagonal
+        i,j = row,column
+        while i<n and j >= 0:
+            if(board[i][j] == "Q"):
+                return False
+            i += 1
+            j -= 1
+        
+        return True
+        
+    
+    def solveQueensRec(self,board,n,output,column):
+        if(column == n):
+            output.append(["".join(i) for i in board])
+            return
+        
+        for row in range(n):
+            if(self.isSafe(board,row,column,n)):
+                board[row][column] = "Q"
+                self.solveQueensRec(board,n,output,column+1)
+                board[row][column] = "."
+        
+        return
+    
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["." for i in range(n)] for i in range(n)]
+        output = []
+        column = 0
+        row = 0
+        self.solveQueensRec(board,n,output,column)
+        return output
